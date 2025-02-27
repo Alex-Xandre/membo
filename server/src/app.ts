86e2402f03c981f534592c03e6e487f6f6e4f828
alexander <micua.161472@vigan.sti.edu.ph>
2025-02-28 02:03:08 +0800

    Config

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,7 +17,7 @@ const port = 5000;
 const server = createServer(app);
 
 const corsOptions = {
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173',"https://membo-nine.vercel.app"],
 };
 
 const io = new Server(server, {
